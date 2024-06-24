b88a03a688c1142689f685350ca5e0d068308b16
Adélaïde Humez <ade@adesky.fr>
2024-06-24 15:26:46 +0200

    Initial Commit

---
+++ Lofty/LoftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoftyApp.swift
+//  Lofty
+//
+//  Created by Adélaïde on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
